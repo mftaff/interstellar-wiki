4c5887ab6c80c51a93d28b134180a273a94063c9
Meir Taffel <artgardening@gmail.com>
2017-03-09 09:52:46 +0000

    sanitize any user inputted html when rendering markdown

--- config/initializers/redcarpet_init.rb
+++ config/initializers/redcarpet_init.rb
@@ -1,1 +1,3 @@
-$markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_block: true, disable_indented_code_blocks: true, strikethrough: true, superscript: true, tables: true)
+renderer = Redcarpet::Render::HTML.new(filter_html: true, safe_links_only: true)
+
+$markdown = Redcarpet::Markdown.new(renderer, fenced_code_block: true, disable_indented_code_blocks: true, strikethrough: true, superscript: true, tables: true)
